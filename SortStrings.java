f60443c2701581cf451f578a8679375aef719b9e
Asuhel2 <47945121+Asuhel2@users.noreply.github.com>
2025-04-19 09:00:15 +0530

    Add files via upload

--- SortStrings.java
+++ SortStrings.java
@@ -2,12 +2,15 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class SortStrings {
 
 	public static void main(String[] args) {
 
-		List<String> names=Arrays.asList("Moon","Sun","Jupiter");
+		List<String> names=Arrays.asList("boost","apple","cup");
 		
 		names.stream().sorted(Comparator.naturalOrder()).forEach(System.out::println);
 
@@ -18,6 +21,37 @@ public class SortStrings {
 		System.out.println(maximum);
 		System.out.println(numbers.stream().reduce(0,(a,b)->a+b));
 		System.out.println(numbers.stream().mapToInt(Integer::intValue).sum());
+		
+		System.out.println("Functional Interface");
+		List<String> strings=Arrays.asList("Zero","Seven","And","One");
+		strings.forEach(string->System.out.println(string));
+	
+		System.out.println("Reverse the List Of Elements");
+		strings.stream().sorted(Comparator.naturalOrder()).forEach(System.out::println);
+		
+		Integer max=numbers.stream().max(Integer::compare).get();
+		System.out.println(max);
+		
+		System.out.println("sum of list of values");
+		System.out.println(numbers.stream().mapToInt(Integer::valueOf).sum());
+		
+
+		String str="Mahammad SUhel";
+		String output=str.chars().distinct().mapToObj(c->String.valueOf((char)c)).collect(Collectors.joining());
+		System.out.println(output);  
+		
+		Stream.iterate(new int[] { 0, 1 }, t -> new int[] { t[1], t[0] + t[1] })
+		.limit(10)
+		.map(t -> t[1])
+		.forEach(System.out::println);
+		
+		System.out.println("Reverse digits and starts with one");
+		
+		int num=5432713;
+		System.out.println(Stream.of(new StringBuilder(String.valueOf(num)).reverse().toString()).mapToInt(Integer::parseInt).findFirst()
+				.orElse(0));
+		
+		System.out.println(numbers.stream().sorted(Comparator.naturalOrder()).findFirst());
 	}
 
 }
